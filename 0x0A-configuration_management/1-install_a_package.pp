92611a91cda3a581bcc7ffa2dedc4b7b6857c439
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-02-24 16:50:03 -0800

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
-#!/usr/bin/env puppet
-
-# Install Flask package with version 2.1.0
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/bin'],
-  refreshonly => true,
-}
-
-file { '/usr/local/bin/flask':
-  ensure => link,
-  target => '/usr/local/bin/flask',
-  require => Exec['install_flask'],
+# Install Flask package
+package {'puppet-lint':
+  ensure   => '2.1.0',
+  procider => 'pip3',
 }
