48afdeb1755c2b74071c86b32c2385ea4f1edc8b
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-23 22:13:05 +0000

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,18 +7,19 @@ import sys
 
 
 if __name__ == "__main__":
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users")
-    user = user.json()
+    users = requests.get(f"https://jsonplaceholder.typicode.com/users")
+    users = user.json()
     todo = requests.get(f"https://jsonplaceholder.typicode.com/todos")
     todoj = todo.json()
     dic = {}
-    for task in todoj:
+    for user in users:
         lis = []
-        task_dic = {"task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": name}
-        lis.append(task_dic)
-    dic[userId] = lis
-    filename = f"{userId}.json"
-    with open(filename, 'w') as file:
+        for task in todoj:
+            if task.get('userId') == user.get('id'):
+                task_dic = {"task": task.get("title"),
+                            "completed": task.get("completed"),
+                            "username": user.get('username')}
+                lis.append(task_dic)
+        dic[user.get('id')] = lis
+    with open('todo_all_employees.json', 'w') as file:
         json.dump(dic, file)
