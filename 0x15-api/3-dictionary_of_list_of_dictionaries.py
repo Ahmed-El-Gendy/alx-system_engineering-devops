13c206272384a211389ac1784a51c0185c287215
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-23 22:48:51 +0000

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" Extend Python script to export data in the JSON format """
+"""Extend Python script to export data in the JSON format"""
 import json
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    users = requests.get(f"https://jsonplaceholder.typicode.com/users")
-    users = users.json()
-    todo = requests.get(f"https://jsonplaceholder.typicode.com/todos")
-    todoj = todo.json()
-    dic = {}
+    # Fetching users and todos data from the API
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    
+    # Dictionary to store all tasks
+    todo_all = {}
+
+    # Iterating through users
     for user in users:
-        lis = []
-        for task in todoj:
+        task_list = []
+        # Iterating through todos
+        for task in todos:
             if task.get('userId') == user.get('id'):
-                task_dic = {"task": task.get("title"),
-                            "completed": task.get("completed"),
-                            "username": user.get('username')}
-                lis.append(task_dic)
-        dic[user.get('id')] = lis
-    with open('todo_all_employees.json', 'w') as file:
-        json.dump(dic, file)
+                task_dict = {
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user.get('username')
+                }
+                task_list.append(task_dict)
+        todo_all[user.get('id')] = task_list
+
+    # Writing the data to JSON file
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todo_all, f)
