57f6cac3120479f28ae18817b4e248b594f8b3a6
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-04-23 10:03:09 +0200

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    userId = 2
+    '''sys.argv[1]'''
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
+    name = user.json().get('name')
+    todo = requests.get(f"https://jsonplaceholder.typicode.com/todos")
+    todoj = todo.json()
+    complete = 0
+    alltasks = 0
+    tasks = []
+    for task in todoj:
+        if task.get('userId') == int(userId):
+            alltasks += 1
+            if task.get('completed'):
+                complete += 1
+                tasks.append(task.get('title'))
+
+    print(f"Employee {name} is done with tasks {complete} / {alltasks}:")
+    for task in tasks:
+        print(f"    {task}")
