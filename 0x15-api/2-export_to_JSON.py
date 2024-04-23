4f1ceaac083c9a83290ffcece438ba647c2b9e6d
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-23 12:40:06 +0000

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" extend Python script to export data in the CSV format """
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
+    name = user.json().get('username')
+    todo = requests.get(f"https://jsonplaceholder.typicode.com/todos")
+    todoj = todo.json()
+    dic = {}
+    lis = []
+    for task in todoj:
+        task_dic = { "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": name}
+        lis.append(task_dic)
+    dic[userId] = lis
+    filename = f"{userId}.csv"
+    with open(filename, 'w') as file:
+        json.dump(dic, file)
