42b4eaa9a1844fc3020d813eb1c6fa3910c2f6be
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-06-18 22:12:56 +0000

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-"""Queries the Reddit API"""
+"""Queries the Reddit API and prints the titles"""
+
+import requests
 
 
 def top_ten(subreddit):
-    """top 10 hot posts"""
-    import requests
-
+    """Fetches and prints the titles of the top 10 hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
-    if response.status_code >= 300:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
         print("None")
-    else:
-        for post in response.json().get("data").get("children"):
-            print(post.get("data").get("title"))
+        return
+
+    try:
+        data = response.json().get("data", {})
+        children = data.get("children", [])
+
+        if not children:
+            print("None")
+            return
+
+        for post in children:
+            print(post.get("data", {}).get("title", ""))
+    except (ValueError, KeyError):
+        print("None")
