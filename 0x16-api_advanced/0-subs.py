c7b0825a557f23bc1f849e97777a2535167aa3aa
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-06-18 21:00:50 +0000

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+import requests
+
+'''
+Queries the Reddit API and returns the number of subscribers
+'''
+
+
+def number_of_subscribers(subreddit):
+    '''
+    number_of_subscribers
+    '''
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
