a24956317cb3db95a6cea58511e8173c18459dab
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-06-18 21:29:40 +0000

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-'''
-Queries the Reddit API and returns the number of subscribers
-'''
+"""Queries the Reddit API"""
 
 
 def number_of_subscribers(subreddit):
+    """number_of_subscribers"""
     import requests
-    '''
-    number_of_subscribers
-    '''
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My-User-Agent"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code >= 300:
         return 0
