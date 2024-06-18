fb2a52bfd7d51dd0b2e1fe35f474aea5d92dac20
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-06-18 21:31:56 +0000

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,11 @@ def number_of_subscribers(subreddit):
     """number_of_subscribers"""
     import requests
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code >= 300:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
     return response.json().get("data").get("subscribers")
