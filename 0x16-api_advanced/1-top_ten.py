7251fa72007bbbf5b85f64937820eb83a0166c42
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-06-18 21:42:57 +0000

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,10 +8,21 @@ def top_ten(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
-    if response.status_code >= 300:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
         print("None")
-    else:
-        for post in response.json().get("data").get("children"):
-            print(post.get("data").get("title"))
+        return
+
+    try:
+        posts = response.json().get("data", {}).get("children", [])
+        if not posts:
+            print("None")
+            return
+
+        for post in posts:
+            print(post.get("data", {}).get("title", ""))
+    except (ValueError, KeyError):
+        print("None")
