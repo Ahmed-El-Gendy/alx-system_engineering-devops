f8f632b0825f0ac40f64898ab559d78948b44c52
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-06-18 21:31:20 +0000

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ def number_of_subscribers(subreddit):
     """number_of_subscribers"""
     import requests
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code >= 300:
